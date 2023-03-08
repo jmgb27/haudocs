187f54320fd4e59ce7322a902078be6c3ffc8176
jarell galura <jarellgaluraa@gmail.com>
2023-03-08 11:15:22 +0800

    shows the errormessages in login page

--- FE/client-app/src/login/auth/Signin.jsx
+++ FE/client-app/src/login/auth/Signin.jsx
@@ -1,177 +1,202 @@
 import React, { useState } from "react";
-import '../LoginForm.css';
+import "../LoginForm.css";
 import Card from "../../components/card/Card";
-import '../../index.css'
-import { useNavigate, Link } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext';
+import "../../index.css";
+import { useNavigate, Link } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
 import { auth, db } from "../../firebase";
-import { signInWithEmailAndPassword, sendEmailVerification, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { useAuthValue } from "../../context/Authvalue";
-import { FaUnlockAlt } from "react-icons/fa"
-import bgimage from "../../assets/bg.jpg"
-import { collection, query, where, getDocs, addDoc, serverTimestamp } from "firebase/firestore/lite";
-import TextField from '@mui/material/TextField';
-import {FcGoogle} from "react-icons/fc"
+import { FaUnlockAlt } from "react-icons/fa";
+import bgimage from "../../assets/bg.jpg";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore/lite";
+import TextField from "@mui/material/TextField";
+import { FcGoogle } from "react-icons/fc";
 
-  function Signin () {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const { signIn } = UserAuth();
-    const [errorMessages, setErrorMessages] = useState(null);  
-    const {setTimeActive} = useAuthValue()
-    const errors = {
-      email: "Invalid email",
-      password: "Invalid password",
-      noUsername: "Please enter your username",
-      noPassword: "Please enter your password",
-      
-    };
+function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { setTimeActive } = useAuthValue();
+  const [errorMessages, setErrorMessages] = useState("");
 
-    const Login = async (e) => {
-      e.preventDefault();
-      signInWithEmailAndPassword(auth, email, password)
-      .catch((errorMessages) => {setErrorMessages(errorMessages)})
-      .then(() => {
-        if(!auth.currentUser.emailVerified) {
-          sendEmailVerification(auth.currentUser)
-          .then(() => {
-            setTimeActive(true)
-            navigate('/verifyemail')
-          })
-        .catch(err => alert(err.message))
-      }else{
-        navigate('/')
+  const Login = async (event) => {
+    event.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      if (!auth.currentUser.emailVerified) {
+        sendEmailVerification(auth.currentUser).then(() => {
+          setTimeActive(true);
+          navigate("/verifyemail");
+        });
+      } else {
+        navigate("/");
       }
-      })
-      .catch(err => setErrorMessages(err.message))
-      try {
-        await signIn(email, password)
-      } catch (e) {
-        console.log(e.message)
+    } catch (error) {
+      switch (error.code) {
+        case "auth/user-not-found":
+          setErrorMessages(
+            "The email address entered does not match any account."
+          );
+          break;
+        case "auth/wrong-password":
+          setErrorMessages("The password entered is incorrect.");
+          break;
+        default:
+          setErrorMessages(error.message);
+          break;
       }
-      if (!email) {
-        // Username input is empty
-        setErrorMessages({ email: "noUsername", message: errors.noUsername });
-        return;
-      }
-  
-      if (!password) {
-        // Password input is empty
-        setErrorMessages({ email: "noPassword", message: errors.noPassword });
-        return;
-      }
-    };
+    }
+  };
 
-    const googleProvider = new GoogleAuthProvider();
-    const signInWithGoogle = async () => {
-      try {
-        const res = await signInWithPopup(auth, googleProvider);
-        const user = res.user;
-        const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        const docs = await getDocs(q);
-        if (docs.docs.length === 0) {
-          await addDoc(collection(db, "users"), {
-            uid: user.uid,
-            name: user.displayName,
-            authProvider: "google",
-            email: user.email,
-            createdAt: serverTimestamp(),
-            role: ""
-          });
-        }
-      } catch (err) {
-        console.error(err);
-        alert(err.message);
+  const googleProvider = new GoogleAuthProvider();
+  const signInWithGoogle = async () => {
+    try {
+      const res = await signInWithPopup(auth, googleProvider);
+      const user = res.user;
+      const q = query(collection(db, "users"), where("uid", "==", user.uid));
+      const docs = await getDocs(q);
+      if (docs.docs.length === 0) {
+        await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          name: user.displayName,
+          authProvider: "google",
+          email: user.email,
+          createdAt: serverTimestamp(),
+          role: "",
+        });
       }
-    };
-  
-/*   useEffect(() => {
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  /*   useEffect(() => {
     if (user != null) {
       navigate('/dashboard');
     }
   }, [user]); */
-  const myStyle={
-    backgroundImage:`url(${bgimage})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    width: '100%',
-    height: '100%',
-    position: 'fixed'
+  const myStyle = {
+    backgroundImage: `url(${bgimage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "100%",
+    position: "fixed",
   };
-  
+  const handleClearError = () => {
+    setErrorMessages("");
+  };
+
   return (
     <div style={myStyle}>
-    <Card>
-    <h1 className="title">Welcome to HAUDOCS!</h1>
-    <form onSubmit={Login}>
-      <div className="inputs_container">
-      <hr className='hrline'></hr>
-      <p className="subtitle mb-6">Login to your account</p>
-      <TextField label="Email Address" className="textfield text-white" 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-/*           sx={{
+      <Card>
+        <h1 className="title">Welcome to HAUDOCS!</h1>
+        <form onSubmit={Login}>
+          <div className="inputs_container">
+            <hr className="hrline"></hr>
+            <p className="subtitle mb-6">Login to your account</p>
+            {errorMessages && (
+              <div className="error-container">
+                <div className="error-message">{errorMessages}</div>
+                <button className="close-button" onClick={handleClearError}>
+                  x
+                </button>
+              </div>
+            )}
+            <TextField
+              label="Email Address"
+              className="textfield text-white"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              /*           sx={{
             "& .MuiInputLabel-root": {color: 'black'},//styles the label
             "& .MuiOutlinedInput-root": {
               "& > fieldset": { borderColor: "black" },
             },
-          }}  */  
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          name="email"
-          autoComplete="email"
-          autoFocus/>
+          }}  */
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
 
-      <TextField label="Password" className="textfield text-white" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-/*             sx={{
+            <TextField
+              label="Password"
+              className="textfield text-white"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              /*             sx={{
             "& .MuiInputLabel-root": {color: 'black',},//styles the label
             "& .MuiOutlinedInput-root": {
             "& > fieldset": { borderColor: "black" },
             },
-            }}  */ 
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
+            }}  */
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
             />
 
-        <div className="text-black underline underline-offset-2 items-center justify-end flex">
-        <FaUnlockAlt/>
-       <Link to="/forgotpassword">
-        Forgot Password?</Link>
-        </div>
-      </div>
-
-      <input type="submit" value="Login" className="login_button" />
-      <div className="hau-signup">
-      <div className="text-black">
-      Dont have an account? Click <Link className="text-black font-semibold underline underline-offset-2 cursor-pointer" to="/signup">here</Link> to register
-      </div>
-      </div>
-      <div className='w-full flex items-center justify-center relative py-6'>
-          <div className='w-full h-[1px] bg-black'></div>
-          <p className='or text-lg absolute'>or</p>
-        </div>
-      <div>
-        <button type="submit" className="google_button" onClick={signInWithGoogle}>
-          <div className="flex items-start justify-start"><FcGoogle size={25}/></div>
-          <div className="text-center">Continue With Google</div>
-        </button>
-      </div>
-      </form>
-  </Card>
-  </div>
-  )
-};
+            <div className="text-black underline underline-offset-2 items-center justify-end flex">
+              <FaUnlockAlt />
+              <Link to="/forgotpassword">Forgot Password?</Link>
+            </div>
+          </div>
+          <input type="submit" value="Login" className="login_button" />
+          <div className="hau-signup">
+            <div className="text-black">
+              Dont have an account? Click{" "}
+              <Link
+                className="text-black font-semibold underline underline-offset-2 cursor-pointer"
+                to="/signup"
+              >
+                here
+              </Link>{" "}
+              to register
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center relative py-6">
+            <div className="w-full h-[1px] bg-black"></div>
+            <p className="or text-lg absolute">or</p>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="google_button"
+              onClick={signInWithGoogle}
+            >
+              <div className="flex items-start justify-start">
+                <FcGoogle size={25} />
+              </div>
+              <div className="text-center">Continue With Google</div>
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+}
 
 export default Signin;
--- FE/client-app/src/login/auth/Signup.jsx
+++ FE/client-app/src/login/auth/Signup.jsx
@@ -5,9 +5,9 @@ import { auth } from "../../firebase";
 import { sendEmailVerification } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuthValue } from "../../context/Authvalue";
-import bgimage from "../../assets/bg.jpg"
+import bgimage from "../../assets/bg.jpg";
 import { registerWithEmailAndPassword } from "../../firebase";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -15,21 +15,21 @@ function Signup() {
   const [errorMessages, setErrorMessages] = useState("");
   const navigate = useNavigate();
   const [confirmPassword, setConfirmPassword] = useState("");
-  const {setTimeActive} = useAuthValue()
+  const { setTimeActive } = useAuthValue();
   const [name, setName] = useState("");
 
   const validatePassword = () => {
-    let isValid = true
-    if (password !== '' && confirmPassword !== ''){
+    let isValid = true;
+    if (password !== "" && confirmPassword !== "") {
       if (password !== confirmPassword) {
-        isValid = false
-        setErrorMessages('Passwords does not match')
+        isValid = false;
+        setErrorMessages("Passwords does not match");
       }
     }
-    return isValid
-  }
+    return isValid;
+  };
 
-/*   const createUser = async () => {
+  /*   const createUser = async () => {
     const usersCollectionRef = collection(db, "users")
     await addDoc(usersCollectionRef, { 
       email: email,
@@ -46,7 +46,7 @@ function Signup() {
     noPassword: "Please enter your password",
   };
 
-/*   const handleSubmit = e => {
+  /*   const handleSubmit = e => {
     // Prevent page from reloading
     e.preventDefault();
     if(validatePassword()) {
@@ -78,19 +78,22 @@ function Signup() {
   }; */
 
   const register = () => {
-    if (validatePassword()) {;
-    registerWithEmailAndPassword(name, email, password)
-    .then(() => {
-      sendEmailVerification(auth.currentUser) 
-      .then(() => {
-        setTimeActive(true)  
-        navigate('/verifyemail')}).catch((err) => alert(err.message))
-    }).catch(err => setErrorMessages(err.message))
+    if (validatePassword()) {
+      registerWithEmailAndPassword(name, email, password)
+        .then(() => {
+          sendEmailVerification(auth.currentUser)
+            .then(() => {
+              setTimeActive(true);
+              navigate("/verifyemail");
+            })
+            .catch((err) => alert(err.message));
+        })
+        .catch((err) => setErrorMessages(err.message));
     }
-    setName('')
-    setEmail('')
-    setPassword('')
-    setConfirmPassword('')
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
 
     if (!email) {
       // Username input is empty
@@ -107,99 +110,115 @@ function Signup() {
 
   // Render error messages
   const renderErrorMsg = (email) =>
-    email=== errorMessages.email && (
+    email === errorMessages.email && (
       <p className="error_msg">{errorMessages.message}</p>
     );
 
-  const myStyle={
-    backgroundImage:`url(${bgimage})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    width: '100%',
-    height: '100%',
-    position: 'fixed'
+  const myStyle = {
+    backgroundImage: `url(${bgimage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "100%",
+    position: "fixed",
   };
 
   return (
     <div style={myStyle}>
-    <Card>
-    <h1 className="title">Sign Up for HAUDOCS <p>Create a free account</p> </h1>
-      <div className="signup-inputs_container">
-      <hr className='hrline'></hr>
-      <p className='text-base mb-4 mt-6 text-black'>Welcome! Please Enter your details</p>
-      {errorMessages && <div className='auth__error'>{errorMessages}</div>}
-        <TextField
-          label="Name" 
-          className="textfield"
-          required
-          variant="outlined"
-          fullWidth
-          id="name"
-          name="name"
-          margin="normal"
-          autoComplete="name"
-          autoFocus
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <Card>
+        <h1 className="title">
+          Sign Up for HAUDOCS <p>Create a free account</p>{" "}
+        </h1>
+        <div className="signup-inputs_container">
+          <hr className="hrline"></hr>
+          <p className="text-base mb-4 mt-6 text-black">
+            Welcome! Please Enter your details
+          </p>
+          {errorMessages && <div className="auth__error">{errorMessages}</div>}
+          <TextField
+            label="Name"
+            className="textfield"
+            required
+            variant="outlined"
+            fullWidth
+            id="name"
+            name="name"
+            margin="normal"
+            autoComplete="name"
+            autoFocus
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <TextField
-          label="Email Address"
-          className="textfield"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="email"
-          name="email"
-          autoComplete="email"
-          required
-          autoFocus
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {renderErrorMsg("username")}
-        {renderErrorMsg("noUsername")}
+          <TextField
+            label="Email Address"
+            className="textfield"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="email"
+            name="email"
+            autoComplete="email"
+            required
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {renderErrorMsg("username")}
+          {renderErrorMsg("noUsername")}
 
-        <TextField
-          className="textfield"
-          margin="normal"
-          label="Password"
-          fullWidth
-          required
-          name="password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {renderErrorMsg("password")}
-        {renderErrorMsg("noPassword")}
+          <TextField
+            className="textfield"
+            margin="normal"
+            label="Password"
+            fullWidth
+            required
+            name="password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {renderErrorMsg("password")}
+          {renderErrorMsg("noPassword")}
 
-        <TextField
-          className="textfield"
-          margin="normal"
-          label="Confirm Password"
-          fullWidth
-          required
-          name="password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={confirmPassword}
-          onChange={e => setConfirmPassword(e.target.value)}
+          <TextField
+            className="textfield"
+            margin="normal"
+            label="Confirm Password"
+            fullWidth
+            required
+            name="password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {renderErrorMsg("password")}
+          {renderErrorMsg("noPassword")}
+        </div>
+        <input
+          onClick={register}
+          type="submit"
+          value="Signup"
+          className="login_button"
         />
-        {renderErrorMsg("password")}
-        {renderErrorMsg("noPassword")}
-      </div>
-      <input onClick={register} type="submit" value="Signup" className="login_button" />
-      <div className='w-full flex items-center justify-center'>
-        <p className='text-sm font-normal text-black mt-5'>Already have an account? <Link to= "/Signin"><a className='font-semibold underline underline-offset-2 cursor-pointer'>Login here</a></Link></p>
-      </div>
-  </Card>
-  </div>
+        <div className="w-full flex items-center justify-center">
+          <p className="text-sm font-normal text-black mt-5">
+            Already have an account?{" "}
+            <Link to="/Signin">
+              <a className="font-semibold underline underline-offset-2 cursor-pointer">
+                Login here
+              </a>
+            </Link>
+          </p>
+        </div>
+      </Card>
+    </div>
   );
-};
+}
 
 export default Signup;
