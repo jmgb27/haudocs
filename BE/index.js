a1a04ff2aab00d2286bda863f4d944c003db25aa
jmgb27 <mark.john6672@gmail.com>
2023-03-11 02:36:03 +0800

    initial bulk uploading of files

--- BE/index.js
+++ BE/index.js
@@ -1,93 +1,145 @@
-const S3 = require('aws-sdk/clients/s3');
-const express = require('express');
+const S3 = require("aws-sdk/clients/s3");
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Create an S3 client
 const s3 = new S3({
-    accessKeyId: 'AKIASURSMXDX3A2STR44',
-    secretAccessKey: '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',
-    region: 'ap-southeast-1'
+    accessKeyId: "AKIASURSMXDX3A2STR44",
+    secretAccessKey: "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",
+    region: "ap-southeast-1",
 });
 
-const BUCKET_NAME = 'hauodocs';
+const BUCKET_NAME = "hauodocs";
 
-app.post('/files', (req, res) => {
-    const file = req.body.file;
-    const fileName = file.name;
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+const deleteFiles = (files) => {
+    files.forEach((file) => {
+        fs.unlink(file.path, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+};
+
+const uploadFiles = (files, folderName) => {
+    files.forEach((file) => {
+        const fileName = file.filename;
+        const fileContent = fs.readFileSync(file.path);
+
+        console.log(fileName);
+
+        // Define the file parameters
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: folderName + "/" + fileName,
+            Body: fileContent,
+        };
+
+        // Upload the file
+        s3.upload(params, (err, data) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+};
+
+//Upload a file to the S3 bucket
+app.post("/files", upload.any(), (req, res) => {
     const folderName = req.headers.filefolder;
-    
-    const fileContent = fs.readFileSync(file.path);
-    
-    // Define the file parameters
-    const params = {
-      Bucket: BUCKET_NAME,
-      Key: folderName + "/" + fileName,
-      Body: fileContent,
-    };
-    
+    if (!req.files) {
+        res.status(400).send({ message: "No files were uploaded." });
+        return;
+    }
+
+    const files = Object.values(req.files);
+
+    console.log(files);
     // Check if the folder exists
     s3.headObject({ Bucket: BUCKET_NAME, Key: folderName }, (err, metadata) => {
-      if (err && err.code === 'NotFound') {
-        // If the folder doesn't exist, create one
-        s3.putObject({ Bucket: BUCKET_NAME, Key: folderName }, (err, data) => {
-          if (err) {
-            res.send(err);
-          }
-          // If the folder is created successfully, upload the file
-          s3.upload(params, (err, data) => {
-            if (err) {
-              res.send(err);
-            }
-            res.send(data);
-          });
-        });
-      } else {
-        // If the folder already exists, upload the file
-        s3.upload(params, (err, data) => {
-          if (err) {
-            res.send(err);
-          }
-          res.send(data);
-        });
-      }
+        if (err && err.code === "NotFound") {
+            // If the folder doesn't exist, create one
+            s3.putObject(
+                { Bucket: BUCKET_NAME, Key: folderName },
+                (err, data) => {
+                    if (err) {
+                        res.send(err);
+                    }
+                    // If the folder is created successfully, upload all the files
+                    uploadFiles(files, folderName);
+                    res.send({
+                        success: true,
+                        message: "All files are uploaded successfully.",
+                    });
+                }
+            );
+            deleteFiles(req.files);
+        } else {
+            // If the folder already exists, upload all the files
+            uploadFiles(files, folderName);
+            res.send({
+                success: true,
+                message: "All files are uploaded successfully.",
+            });
+            deleteFiles(req.files);
+        }
     });
-  });  
+});
 
-  app.get('/files/:key', (req, res) => {
+// Download File from the S3 bucket
+app.get("/files/:key", (req, res) => {
     // Define the file parameters
     const params = {
-      Bucket: BUCKET_NAME,
-      Key: req.params.key
+        Bucket: BUCKET_NAME,
+        Key: req.params.key,
     };
-  
+
     // Retrieve the file
     s3.getObject(params, (err, data) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-  
-      res.send(data.Body);
-    });
-  });
+        if (err) {
+            return res.status(500).send(err);
+        }
 
-  app.delete('/files/:key', (req, res) => {
+        res.send(data.Body);
+    });
+});
+
+// Delete file from the S3 bucket
+app.delete("/files/:key", (req, res) => {
     // Define the file parameters
     const params = {
-      Bucket: BUCKET_NAME,
-      Key: req.params.key
+        Bucket: BUCKET_NAME,
+        Key: req.params.key,
     };
-  
+
     // Delete the file
     s3.deleteObject(params, (err, data) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-  
-      res.send(data);
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.send(data);
     });
-  });
+});
 
 app.listen(8080, () => {
-  console.log('Server listening on port 8080');
+    console.log("Server listening on port 8080");
 });
