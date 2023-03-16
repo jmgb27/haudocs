a26bbe1a87a60c8232e8378635e4b1ab6a104062
jarell galura <jarellgaluraa@gmail.com>
2023-03-16 22:10:09 +0800

    admin page changes

--- FE/client-app/src/adminpages/submissions/applicants/modals/applicanttabmodal/Finaltab.jsx
+++ FE/client-app/src/adminpages/submissions/applicants/modals/applicanttabmodal/Finaltab.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
 import {
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
+  Typography,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -20,20 +21,61 @@ import { useNavigate } from "react-router-dom";
 
 const Finaltab = (props) => {
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
   const { handleCloseModal } = props;
   const [protocolNumber, setProtocolNumber] = React.useState("");
   const [reviewType, setReviewType] = React.useState("");
   const [isCheckedHau, setIsCheckedHau] = React.useState(false);
   const [isCheckedOthers, setIsCheckedOthers] = React.useState(false);
   const [assignTo, setAssignTo] = React.useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isAnyCheckboxSelected, setIsAnyCheckboxSelected] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const formRef = useRef(null);
+
+  const rows = [
+    {
+      id: "1",
+      documentname: "HAU-IRB FORM 3.7(A): Final Report Form",
+      sentby: "Stephanie David",
+      datesent: "January 28, 2023",
+    },
+  ];
+
+  const checkFormValidity = () => {
+    const requiredFields = [protocolNumber, reviewType, assignTo];
+    const isAllFieldsFilledOut = requiredFields.every((field) => !!field);
+    const isAnyCheckboxSelected = isCheckedHau || isCheckedOthers;
+    const isCheckboxSelectedAndValid =
+      isAnyCheckboxSelected && selectedRows.length > 0;
+
+    setIsFormValid(
+      isAllFieldsFilledOut && isCheckboxSelectedAndValid && !!protocolNumber
+    );
+  };
+
+  useEffect(() => {
+    setIsAnyCheckboxSelected(
+      isCheckedHau || isCheckedOthers || selectedRows.length > 0
+    );
+    checkFormValidity();
+  }, [
+    isCheckedHau,
+    isCheckedOthers,
+    isAnyCheckboxSelected,
+    selectedRows,
+    rows,
+  ]);
 
   const handleProtocolNumberChange = (event) => {
     setProtocolNumber(event.target.value);
+    checkFormValidity();
   };
 
   const handleReviewTypeChange = (event) => {
     setReviewType(event.target.value);
+    checkFormValidity();
   };
 
   const handleCheckboxChange = (event) => {
@@ -46,18 +88,12 @@ const Finaltab = (props) => {
       setIsCheckedOthers(checked);
       setIsCheckedHau(false); // uncheck Hau checkbox
     }
+    checkFormValidity();
   };
 
   const handleAssignToChange = (event) => {
     setAssignTo(event.target.value);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+    checkFormValidity();
   };
 
   function handleDownload(id) {
@@ -66,15 +102,27 @@ const Finaltab = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    navigate("/adminapplication");
-    console.log({
-      protocolNumber,
-      reviewType,
-      assignTo,
-      isCheckedHau,
-      isCheckedOthers,
-    });
+    formRef.current.reset();
+    checkFormValidity();
+    if (isFormValid && isAnyCheckboxSelected) {
+      setShowConfirmation(false);
+      setShowSuccess(true);
+      console.log({
+        protocolNumber,
+        reviewType,
+        assignTo,
+        isCheckedHau,
+        isCheckedOthers,
+      });
+    } else {
+      alert("Please select at least one checkbox");
+    }
   };
+
+  const handleClose = () => {
+    navigate("/adminapplication");
+  };
+
   const downloadStyle = {
     color: "maroon",
   };
@@ -95,14 +143,6 @@ const Finaltab = (props) => {
     },
   ];
 
-  const rows = [
-    {
-      id: "1",
-      documentname: "HAU-IRB FORM 3.7(A): Final Report Form",
-      sentby: "Stephanie David",
-      datesent: "January 28, 2023",
-    },
-  ];
   const closeStyle = {
     color: "maroon",
     borderColor: "maroon",
@@ -121,82 +161,120 @@ const Finaltab = (props) => {
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        onSelectionModelChange={(newSelection) => {
+          setSelectedRows(newSelection);
+          setIsAnyCheckboxSelected(newSelection.length > 0);
+          checkFormValidity();
+        }}
       />
-      <Box sx={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={isCheckedHau}
-              onChange={handleCheckboxChange}
-              name="Hau"
-            />
-          }
-          label="Hau"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={isCheckedOthers}
-              onChange={handleCheckboxChange}
-              name="Others"
-            />
-          }
-          label="Others"
-        />
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-        <TextField
-          label="Protocol Number"
-          value={protocolNumber}
-          onChange={handleProtocolNumberChange}
-        />
-        <Box sx={{ display: "flex", gap: "1rem" }}>
-          <FormControl sx={{ width: "100%" }}>
-            <InputLabel>Review Type</InputLabel>
-            <Select value={reviewType} onChange={handleReviewTypeChange}>
-              <MenuItem value="Type A">Full Board Review</MenuItem>
-              <MenuItem value="Type B">Expedited Review</MenuItem>
+      <Box
+        ref={formRef}
+        component="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setShowConfirmation(true);
+        }}
+      >
+        <Box sx={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={isCheckedHau}
+                onChange={handleCheckboxChange}
+                name="Hau"
+              />
+            }
+            label="Hau"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={isCheckedOthers}
+                onChange={handleCheckboxChange}
+                name="Others"
+              />
+            }
+            label="Others"
+          />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <TextField
+            required
+            label="Protocol Number"
+            value={protocolNumber}
+            onChange={handleProtocolNumberChange}
+          />
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            <FormControl sx={{ width: "100%" }}>
+              <InputLabel>Review Type</InputLabel>
+              <Select value={reviewType} onChange={handleReviewTypeChange}>
+                <MenuItem value="Type A">Full Board Review</MenuItem>
+                <MenuItem value="Type B">Expedited Review</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <FormControl sx={{ minWidth: 120, marginBottom: "2rem" }}>
+            <InputLabel>Assign To</InputLabel>
+            <Select value={assignTo} onChange={handleAssignToChange}>
+              <MenuItem value="Person A">Person A</MenuItem>
+              <MenuItem value="Person B">Person B</MenuItem>
+              <MenuItem value="Person C">Person C</MenuItem>
             </Select>
           </FormControl>
         </Box>
-        <FormControl sx={{ minWidth: 120, marginBottom: "2rem" }}>
-          <InputLabel>Assign To</InputLabel>
-          <Select value={assignTo} onChange={handleAssignToChange}>
-            <MenuItem value="Person A">Person A</MenuItem>
-            <MenuItem value="Person B">Person B</MenuItem>
-            <MenuItem value="Person C">Person C</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <div className="flex items-end justify-end space-x-2 pb-[2rem]">
-        <Button
-          style={closeStyle}
-          onClick={handleCloseModal}
-          variant="outlined"
-        >
-          Close
-        </Button>
-        <Button onClick={handleOpen} style={submitStyle} variant="contained">
-          Forward
-        </Button>
-      </div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Are you sure you want to submit?</DialogTitle>
-        <DialogContent>{/* Add any additional content here */}</DialogContent>
-        <DialogActions className="space-x-4">
-          <Button variant="outlined" style={closeStyle} onClick={handleClose}>
-            Cancel
+        <div className="flex items-end justify-end space-x-2 pb-[2rem]">
+          <Button
+            style={closeStyle}
+            onClick={handleCloseModal}
+            variant="outlined"
+          >
+            Close
           </Button>
           <Button
+            id="sub"
+            type="submit"
             variant="contained"
-            sx={{ width: "100px" }}
-            style={submitStyle}
-            onClick={handleSubmit}
+            disabled={!isFormValid}
           >
-            Yes
+            Forward
           </Button>
-        </DialogActions>
-      </Dialog>
+        </div>
+        <Dialog
+          open={showConfirmation}
+          onClose={() => setShowConfirmation(false)}
+        >
+          <DialogTitle>Confirm Submit</DialogTitle>
+          <DialogContent>
+            <Typography variant="body1">
+              Are you sure you want to forward the form?
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              sx={{ color: "maroon" }}
+              onClick={() => setShowConfirmation(false)}
+            >
+              Cancel
+            </Button>
+            <Button sx={{ color: "maroon" }} onClick={handleSubmit} autoFocus>
+              Forward
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Dialog open={showSuccess} onClose={() => setShowSuccess(false)}>
+          <DialogTitle>Success!</DialogTitle>
+          <DialogContent>
+            <Typography variant="body1">
+              You have successfully transferred the files.
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button sx={{ color: "maroon" }} onClick={handleClose}>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
     </div>
   );
 };
